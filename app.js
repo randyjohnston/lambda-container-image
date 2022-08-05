911f3cd668aa70027dd780d28fac50922acc2b57
Randy Johnston <randy.johnston@snyk.io>
2022-08-05 16:44:25 -0500

    Add signature verification and log event from Snyk webhooks

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-exports.handler =  async function(event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
+const { createHmac } = require('crypto');
+
+const secret = process.env.secret;
+
+exports.handler = async function (event, context) {
+  if (verifySignature(event, secret)) {
+    console.log(JSON.stringify(event));
+  } else {
+    console.error('Invalid signature');
   }
+  return context.logStreamName;
+}
+
+function verifySignature(event, secret) {
+  const hmac = createHmac('sha256', secret);
+  const buffer = JSON.stringify(JSON.parse(event.body));
+  hmac.update(buffer, 'utf8');
+
+  const signature = `sha256=${hmac.digest('hex')}`;
+  console.log('Received signature: ', event.headers['x-hub-signature']);
+  console.log('Calculated signature: ', signature);
+
+  return signature === event.headers['x-hub-signature'];
+}
